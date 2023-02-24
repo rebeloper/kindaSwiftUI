21d62d36b5427378e6744e4f76016cc99516ede9
rebeloper <therebeloper@gmail.com>
2023-02-24 21:31:45 +0200

    log

---
+++ Sources/kindaSwiftUI247.xcframework/ios-arm64/kindaSwiftUI247.framework/Headers/kindaSwiftUI247.h
@@ -0,0 +1,18 @@
+//
+//  kindaSwiftUI247.h
+//  kindaSwiftUI247
+//
+//  Created by Alex Nagy on 21.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kindaSwiftUI247.
+FOUNDATION_EXPORT double kindaSwiftUI247VersionNumber;
+
+//! Project version string for kindaSwiftUI247.
+FOUNDATION_EXPORT const unsigned char kindaSwiftUI247VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kindaSwiftUI247/PublicHeader.h>
+
+
